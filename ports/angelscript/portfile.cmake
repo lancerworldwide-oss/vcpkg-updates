94299479d8fefbe2f1dc28eedce6cd8ff928d1e0
Dean M. Sands III <dean.sands@lancerworldwide.com>
2025-02-20 08:59:58 -0600

    NGSW-443: Re-added cpptotp

--- ports/angelscript/portfile.cmake
+++ ports/angelscript/portfile.cmake
@@ -5,5 +5,15 @@ vcpkg_from_git(
     PATCHES lancer_build.patch
 )
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
--- ports/cpptotp/portfile.cmake
+++ ports/cpptotp/portfile.cmake
@@ -5,5 +5,18 @@ vcpkg_from_git(
     PATCHES CMakeLists.patch
 )
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/cpptotp-config.cmake.in" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
